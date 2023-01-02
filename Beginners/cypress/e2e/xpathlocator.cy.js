c357c77ba6cc63efb1e789cafc60a3214d4890e5
officialpradip <study.pradipthapa@gmail.com>
2023-01-02 15:25:07 +0545

    chained xpath selector

--- Beginners/cypress/e2e/xpathlocator.cy.js
+++ Beginners/cypress/e2e/xpathlocator.cy.js
@@ -3,4 +3,9 @@ describe("Xpath Locator", ()=>{
         cy.visit('https://demo.opencart.com/')
         cy.xpath("//div[@id='content']//div[@class='row']/div").should('have.length',4)
     })
+
+    it("chained Xpath test",()=>{
+        cy.visit('https://demo.opencart.com/')
+        cy.xpath("//div[@id='content']//div[@class='row']").xpath('./div').should('have.length',4)
+    })
 })
