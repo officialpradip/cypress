12076f6eb6e56a2a81c36cec584829537c57bff6
officialpradip <study.pradipthapa@gmail.com>
2023-01-04 16:00:51 +0545

    assertions

--- Beginners/cypress/e2e/assertions.cy.js
+++ Beginners/cypress/e2e/assertions.cy.js
@@ -9,7 +9,11 @@ describe('Assertions Demo',()=>{
         //and
        cy.url().should('include','opensource-demo.orangehrmlive').and('contain','orange').and('not.contain','ORANGES')
        cy.title().should('include','OrangeHRM').and('eq','OrangeHRM').and('contain','HRM')
-       cy.get('.orangehrm-login-branding > img').should('be.visible').and('exist')
+       cy.get('.orangehrm-login-branding > img').should('be.visible').and('exist') //exist or not
+       cy.xpath('//a').should('have.length',5) //links number
+       cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+       cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').should('have.value','Admin')
+       cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
 
     })
 })
