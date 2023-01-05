709bdf4ab4727e73cee6ed109958f8b9d2b69a8b
officialpradip <study.pradipthapa@gmail.com>
2023-01-05 12:23:43 +0545

    accessing elements

---
+++ Beginners/cypress/e2e/accessingelements.cy.js
@@ -0,0 +1,7 @@
+/// <reference types="Cypress"/>
+describe("Accessing Elements",()=>{
+    it("First",()=>{
+        cy.visit('https://itera-qa.azurewebsites.net/home/automation')
+        
+    })
+})
