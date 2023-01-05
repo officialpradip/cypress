38e12027e9b54f57fc264442944d89ca400bba7e
officialpradip <study.pradipthapa@gmail.com>
2023-01-05 12:46:08 +0545

    radio button and checkbox

--- Beginners/cypress/e2e/accessingelements.cy.js
+++ Beginners/cypress/e2e/accessingelements.cy.js
@@ -1,7 +1,24 @@
 /// <reference types="Cypress"/>
 describe("Accessing Elements",()=>{
-    it("First",()=>{
+    it("Radio Button Test",()=>{
         cy.visit('https://itera-qa.azurewebsites.net/home/automation')
+        cy.get("input#female").should('be.visible').and('exist')
+        cy.get("input#male").should('be.visible').and('exist')
+        //selecting radio button
+        cy.get("input#male").check().should('be.checked')
+        cy.get("input#female").should('not.be.checked')
+
+        cy.get("input#female").should('not.be.checked')
+        cy.get("input#male").check().should('be.checked')
         
+    })
+
+    it("Checkboxes Test",()=>{
+        cy.visit('https://itera-qa.azurewebsites.net/home/automation')
+        cy.get("input#monday").should('be.visible').and('exist')
+        //selecting single check box
+        cy.get("input#monday").check().should('be.checked')
+        //uncheck selected box
+        cy.get("input#monday").uncheck().should('not.be.checked')
     })
 })
