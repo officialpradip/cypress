aabe6b4c698d91c026992a0501a9fc72e65b8a39
officialpradip <study.pradipthapa@gmail.com>
2023-01-06 15:10:04 +0545

    alert confirm

--- Beginners/cypress/e2e/alerts.cy.js
+++ Beginners/cypress/e2e/alerts.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress"/>
 
 describe("Alerts",()=>{
-    it("Alert Ok Button",()=>{
+    it.skip("Alert Ok Button",()=>{
         cy.visit('https://the-internet.herokuapp.com/javascript_alerts')
         cy.get("button[onclick='jsAlert()']").click()
         cy.on('window:alert',(t)=>{
@@ -11,6 +11,12 @@ describe("Alerts",()=>{
 
     })
     it("Alert Ok and Cancle Button",()=>{
+        cy.visit('https://the-internet.herokuapp.com/javascript_alerts')
+        cy.get("button[onclick='jsConfirm()']").click();
+        cy.on('window:confirm',(t)=>{
+            expect(t).to.contains('I am a JS Confirm')
+        })
+        cy.get("#result").should('have.text','You clicked: Ok')
 
     })
 
