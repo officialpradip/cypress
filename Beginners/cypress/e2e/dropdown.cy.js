cb0d1240381a9c5b800793ddf51a320d793bc06d
officialpradip <study.pradipthapa@gmail.com>
2023-01-06 10:03:12 +0545

    dropdown all

--- Beginners/cypress/e2e/dropdown.cy.js
+++ Beginners/cypress/e2e/dropdown.cy.js
@@ -12,10 +12,23 @@ describe("Dropdown",()=>{
         cy.get("#select2-billing_country-container").should('have.text','Nepal')
     })
 
-    it("DropDown Auto Suggest",()=>{
+    it.skip("DropDown Auto Suggest",()=>{
         cy.visit('https://www.wikipedia.org/')
         cy.get("#searchInput").type('Nepal')
         cy.get(".suggestion-text").contains('Nepal Communist Party').click()
+        
+    })
+
+    it("Dynamic DropDown",()=>{
+        cy.visit('https://www.google.com/')
+        cy.get("input[name='q']").type('cypress')
+        cy.wait(3000)
+        cy.get("div.s49v2>span").should('have.length',9)
+        cy.get("div.s49v2>span").each(($el,index,$list)=>{
+            if($el.text()=='nepali'){
+                cy.wrap($el).click()
+            }
+        })
         
     })
 })
