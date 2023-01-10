68c57b35e31ee4d0aa669b1362760e6fca2d031f
officialpradip <study.pradipthapa@gmail.com>
2023-01-10 15:11:34 +0545

    first approach for tab

--- Beginners/cypress/e2e/child.cy.js
+++ Beginners/cypress/e2e/child.cy.js
@@ -1,1 +1,12 @@
 /// <reference types="Cypress"/>
+
+describe("New Tab",()=>{
+    it("First Approach",()=>{
+        cy.visit('https://the-internet.herokuapp.com/windows')
+        cy.get('.example >a').invoke('removeAttr','target').click()
+         //remove target="_blank" using invoke('first paramater','secondparameter')
+        cy.url().should('include','https://the-internet.herokuapp.com/windows/new')
+        cy.wait(5000)
+        cy.go('back') //back to parent tab
+    })
+})
