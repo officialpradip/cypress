91dbe883fb9928b426e1512c9be6b346ce9bd196
officialpradip <study.pradipthapa@gmail.com>
2023-01-10 15:58:15 +0545

    second approach

--- Beginners/cypress/e2e/iframe.cy.js
+++ Beginners/cypress/e2e/iframe.cy.js
@@ -1,11 +1,18 @@
 /// <reference types="Cypress"/>
 
 describe("iframe",()=>{
-    it("first",()=>{
+    it.skip("first",()=>{
         cy.visit('https://the-internet.herokuapp.com/iframe')
        let iframe= cy.get('#mce_0_ifr').its('0.contentDocument.body').should('be.visible').then(cy.wrap)
        iframe.clear().type("Automation Testing {ctrl+a}")
        cy.get('[aria-label="Bold"]').click()
+
+    })
+    it.skip("Second Approach custom command",()=>{
+        cy.visit('https://the-internet.herokuapp.com/iframe')
+        cy.getIframe('#mce_0_ifr').clear().type("Automation Testing {ctrl+a}")
+       cy.get('[aria-label="Bold"]').click()
+       
 
     })
 })
