a6fe74f8e4ca9ea4684e42fef08f3a97f9a18c27
officialpradip <study.pradipthapa@gmail.com>
2023-01-10 15:47:48 +0545

    first approach

--- Beginners/cypress/e2e/iframe.cy.js
+++ Beginners/cypress/e2e/iframe.cy.js
@@ -3,6 +3,9 @@
 describe("iframe",()=>{
     it("first",()=>{
         cy.visit('https://the-internet.herokuapp.com/iframe')
+       let iframe= cy.get('#mce_0_ifr').its('0.contentDocument.body').should('be.visible').then(cy.wrap)
+       iframe.clear().type("Automation Testing {ctrl+a}")
+       cy.get('[aria-label="Bold"]').click()
 
     })
 })
