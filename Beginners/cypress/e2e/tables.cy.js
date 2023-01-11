4a1ada23734bfda4a3247cb80a0fc726b82d4b08
officialpradip <study.pradipthapa@gmail.com>
2023-01-11 15:34:20 +0545

    no of rows columns

--- Beginners/cypress/e2e/tables.cy.js
+++ Beginners/cypress/e2e/tables.cy.js
@@ -5,9 +5,17 @@ describe("Table Cypress",()=>{
         cy.visit("https://demo.opencart.com/admin/index.php")
         cy.get("#input-username").type('demo')
         cy.get("#input-password").type('demo{enter}')
+        //alert
+        cy.get(".btn-close").click()
+        cy.wait(2000)
+        cy.get("#menu-customer").click()
+        cy.get("#menu-customer>ul>li:first-child").click() //customer child/sub items
+        
         
     })
-    it("",()=>{
+    it("check number of rows and columns",()=>{
+        cy.get('.table.table-bordered.table-hover>tbody>tr').should('have.length',10)
+        cy.get(".table.table-bordered.table-hover>thead>tr>td").should('have.length',7)
 
     })
 })
