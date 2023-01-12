f97e8a4d98c146cd011bb3459e64b7431df2fd4a
officialpradip <study.pradipthapa@gmail.com>
2023-01-12 15:14:09 +0545

    file handling setup

---
+++ Beginners/cypress/e2e/filehandling.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="Cypress"/>
+describe("File Handling",()=>{
+    it('Single file upload',()=>{
+
+    })
+    it('File Upload - Rename',()=>{
+
+    })
+    it('File Upload - Drag and Drop',()=>{
+
+    })
+    it("Multiple Files Upload",()=>{
+
+    })
+    it("File Upload -Shadow Dom",()=>{
+        
+    })
+})
