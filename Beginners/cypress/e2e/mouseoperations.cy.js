b11d73750cfcb320bf65281e1dd27a4a31f1b66f
officialpradip <study.pradipthapa@gmail.com>
2023-01-12 13:53:57 +0545

    mouse hover

--- Beginners/cypress/e2e/mouseoperations.cy.js
+++ Beginners/cypress/e2e/mouseoperations.cy.js
@@ -1,6 +1,11 @@
 /// <reference types="Cypress"/>
 describe("Mouse Operations",()=>{
-    it("Mouse Hover",()=>{
+    it.only("Mouse Hover",()=>{
+        cy.visit("https://demo.opencart.com/")
+        cy.wait(4000)
+        
+        cy.get('.nav > :nth-child(1) > .dropdown-toggle').trigger('mouseover').click()
+        cy.get("div[class='dropdown-menu show'] li:nth-child(2) a:nth-child(1)").should('contain','Mac').and('be.visible').click()
 
     })
 
@@ -15,6 +20,6 @@ describe("Mouse Operations",()=>{
 
     })
     it("Scrolling",()=>{
-        
+
     })
 })
