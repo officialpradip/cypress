535bb031114515c92ca68252fbc17696dd7d3801
officialpradip <study.pradipthapa@gmail.com>
2023-01-12 13:38:16 +0545

    mouse operation setup

--- Beginners/cypress/e2e/mouseoperations.cy.js
+++ Beginners/cypress/e2e/mouseoperations.cy.js
@@ -1,4 +1,20 @@
 /// <reference types="Cypress"/>
 describe("Mouse Operations",()=>{
-    
+    it("Mouse Hover",()=>{
+
+    })
+
+    it("Right Click",()=>{
+
+    })
+
+    it("Double Click",()=>{
+
+    })
+    it("Drag and Drop using plugin",()=>{
+
+    })
+    it("Scrolling",()=>{
+        
+    })
 })
