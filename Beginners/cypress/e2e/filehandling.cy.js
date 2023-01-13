026fde2a82dc5d9c5adfabbfbd940a7129da5560
officialpradip <study.pradipthapa@gmail.com>
2023-01-13 09:12:08 +0545

    rename upload file

--- Beginners/cypress/e2e/filehandling.cy.js
+++ Beginners/cypress/e2e/filehandling.cy.js
@@ -2,7 +2,7 @@
 import 'cypress-file-upload'
 //install plugin using : npm install --save–dev cypress-file-upload
 describe("File Handling",()=>{
-    it.only('Single file upload',()=>{
+    it('Single file upload',()=>{
         cy.visit('https://the-internet.herokuapp.com/upload')
         cy.get("#file-upload").attachFile('Capture.PNG')
         cy.get("#file-submit").click()
@@ -11,7 +11,12 @@ describe("File Handling",()=>{
         
 
     })
-    it('File Upload - Rename',()=>{
+    it.only('File Upload - Rename',()=>{
+        cy.visit('https://the-internet.herokuapp.com/upload')
+        cy.get("#file-upload").attachFile({filePath:'Capture.PNG',fileName:'Test.PNG'})
+        cy.get("#file-submit").click()
+        cy.wait(5000)
+        cy.get("div[class='example'] h3").should('have.text','File Uploaded!')
 
     })
     it('File Upload - Drag and Drop',()=>{
