a0ca897e8caaec82fa37567e16b0b403ef6f0217
officialpradip <study.pradipthapa@gmail.com>
2023-01-14 10:04:36 +0545

    screenshort

---
+++ Beginners/cypress/e2e/capturescreenshots.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress"/>
+ 
+describe("Screenshot and Videos",()=>{
+    it("Test",()=>{
+        cy.visit("https://demo.opencart.com/")
+        cy.screenshot("homepage")
+        cy.wait(5000)
+        cy.get("img[title='Your Store']").screenshot('logo')
+    })
+})
