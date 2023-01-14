615a6fb292741464e045fccac85809f290daeb40
officialpradip <study.pradipthapa@gmail.com>
2023-01-14 09:24:45 +0545

    hooks and tags

--- Beginners/cypress/e2e/hooksnrags.cy.js
+++ Beginners/cypress/e2e/hooksnrags.cy.js
@@ -1,12 +1,36 @@
 /// <reference types="Cypress"/>
+
+// Hooks provided by cypress
+// before 
+// after
+// beforeEach -- executed multiple times before it block
+// afterEach -- executed multiple times after it block
+
 describe("hooks and tags",()=>{
+    before("Before starting all",()=>{
+        cy.log("=======Lunch App====")
+    })
+    after("After completing all",()=>{
+        cy.log("=======After success Lunch App====")
+    })
+
+    beforeEach("Before Each",()=>{
+        cy.log("=======Logging====")
+    })
+    afterEach("After Each",()=>{
+        cy.log("=======Log Out====")
+    })
+
     it("search",()=>{
+        cy.log("====searching====")
 
     })
     it("advance search",()=>{
+        cy.log("====advance searching====")
         
     })
     it("listing products",()=>{
+        cy.log("====listing products====")
         
     })
 })
