567fbf708c5ddfa7dd9dbe5227e3a362eec963ef
officialpradip <study.pradipthapa@gmail.com>
2023-01-16 16:16:04 +0545

    first page object approach

--- Beginners/cypress/e2e/pageobjectmodelpattern.cy.js
+++ Beginners/cypress/e2e/pageobjectmodelpattern.cy.js
@@ -1,5 +1,5 @@
 /// <reference types="Cypress"/>
-
+import Login from "../PageObject/Login2"
 describe("Page Object Model Pattern",()=>{
     //general approach
     it("Login",()=>{
@@ -8,7 +8,15 @@ describe("Page Object Model Pattern",()=>{
         cy.get("input[placeholder='Password']").type('admin123')
         cy.get("button[type='submit']").click()
         cy.get(".oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module").should('have.text','Dashboard')
+    })
+    it.only("Login following Page Object model",()=>{
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 
-
+        const ln=new Login();
+        ln.setUserName("Admin")
+        ln.setUserPassword("admin123")
+        ln.loginButton();
+        ln.verfiyLogin();
+        
     })
 })
