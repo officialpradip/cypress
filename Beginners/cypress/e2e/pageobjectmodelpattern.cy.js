a3a993aa479d5c3bdca3c2b6f9d5a54d935a0499
officialpradip <study.pradipthapa@gmail.com>
2023-01-16 16:02:19 +0545

    general approach

--- Beginners/cypress/e2e/pageobjectmodelpattern.cy.js
+++ Beginners/cypress/e2e/pageobjectmodelpattern.cy.js
@@ -1,7 +1,14 @@
 /// <reference types="Cypress"/>
 
 describe("Page Object Model Pattern",()=>{
+    //general approach
     it("Login",()=>{
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.get("input[placeholder='Username']").type('Admin')
+        cy.get("input[placeholder='Password']").type('admin123')
+        cy.get("button[type='submit']").click()
+        cy.get(".oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module").should('have.text','Dashboard')
+
 
     })
 })
