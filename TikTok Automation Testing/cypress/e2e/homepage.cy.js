100f47bc69c33a1be15389b68884e077dc526719
officialpradip <study.pradipthapa@gmail.com>
2023-01-21 09:09:51 +0545

    Instagram

--- TikTok Automation Testing/cypress/e2e/homepage.cy.js
+++ TikTok Automation Testing/cypress/e2e/homepage.cy.js
@@ -1,5 +1,9 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+/// <reference types="Cypress"/>
+describe('Instagram HomePage', () => {
+    beforeEach("Launching App",()=>{
+        cy.visit('https://www.instagram.com/')
+    })
+    it('Home Page', () => {
+      cy.get('._aacl > .x1i10hfl').click()
+    })
   })
-})
