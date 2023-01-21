1415614df03888b573eee64f6903e1f4d3ecc578
Pradip Thapa <49484626+officialpradip@users.noreply.github.com>
2023-01-21 09:27:03 +0545

    remove tiktok

--- TikTok Automation Testing/cypress/e2e/homepage.cy.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="Cypress"/>
-describe('Instagram HomePage', () => {
-    beforeEach("Launching App",()=>{
-        cy.visit('https://www.instagram.com/')
-    })
-    it('Home Page', () => {
-      cy.get('._aacl > .x1i10hfl').click()
-    })
-  })
