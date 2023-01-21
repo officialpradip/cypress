17ee64613dacad7345ecabc5a0242f1433cb8f3f
officialpradip <study.pradipthapa@gmail.com>
2023-01-21 09:38:38 +0545

    home page visibility

--- Instagram Automation Testing/cypress/e2e/homepage.cy.js
+++ Instagram Automation Testing/cypress/e2e/homepage.cy.js
@@ -2,10 +2,15 @@
 describe('Instagram HomePage', () => {
     beforeEach("Launching App",()=>{
         cy.visit('https://www.instagram.com/')
+        cy.get('._aacl > .x1i10hfl').click()
+        
     })
-    it('Home Page', () => {
-      cy.get('._aacl > .x1i10hfl').click()
-      cy.title().should('eq','Instagram')
-      cy.screenshot('HomePage')
+
+    it("Visibility of every text",()=>{
+      cy.wait(5000)
+      cy.get('._abal > div > img').should('be.visible')
+      cy.get('._ab37').should('be.visible')
+      cy.get('[aria-label="Get it on Google Play"] > ._aa5q').should('be.visible')
+      cy.get('[aria-label="Get it from Microsoft"] > ._aa5q').should('be.visible')
     })
   })
