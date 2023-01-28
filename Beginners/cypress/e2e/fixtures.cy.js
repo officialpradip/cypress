57ae6902f809aece5cac5b735def844b067630c4
officialpradip <study.pradipthapa@gmail.com>
2023-01-28 08:49:30 +0545

    fixture

--- Beginners/cypress/e2e/fixtures.cy.js
+++ Beginners/cypress/e2e/fixtures.cy.js
@@ -1,7 +1,12 @@
 /// <reference types="Cypress"/>
 describe("Fixtures",()=>{
     //direct access
-    it("Fixtures demo test",()=>{
+    it.only("Fixtures demo test",()=>{
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.get("input[placeholder='Username']").type("Admin")
+        cy.get("input[placeholder='Password']").type("admin123")
+        cy.get("button[type='submit']").click()
+        cy.get(':nth-child(2) > .oxd-main-menu-item').contains('PIM')
 
     })
     it("Fixtures Hooks",()=>{
