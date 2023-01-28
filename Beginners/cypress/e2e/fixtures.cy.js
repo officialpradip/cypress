9e40edef7a52e1d618ed097312d3390ec992b381
officialpradip <study.pradipthapa@gmail.com>
2023-01-28 09:02:50 +0545

    fixture hooks

--- Beginners/cypress/e2e/fixtures.cy.js
+++ Beginners/cypress/e2e/fixtures.cy.js
@@ -1,15 +1,34 @@
 /// <reference types="Cypress"/>
 describe("Fixtures",()=>{
     //direct access
-    it.only("Fixtures demo test",()=>{
+    it.skip("Fixtures demo test",()=>{
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-        cy.get("input[placeholder='Username']").type("Admin")
-        cy.get("input[placeholder='Password']").type("admin123")
-        cy.get("button[type='submit']").click()
-        cy.get(':nth-child(2) > .oxd-main-menu-item').contains('PIM')
+
+        cy.fixture('orangehrm.json').then((data)=>{
+            cy.get("input[placeholder='Username']").type(data.username)
+            cy.get("input[placeholder='Password']").type(data.password)
+            cy.get("button[type='submit']").click()
+            cy.get(':nth-child(2) > .oxd-main-menu-item').contains(data.menu_expected)
+
+        })
+        
 
     })
+    let userdata;
+    before("Required",()=>{
+        cy.fixture('orangehrm.json').then((data)=>{
+            userdata=data;
+
+        })
+    })
     it("Fixtures Hooks",()=>{
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.get("input[placeholder='Username']").type(userdata.username)
+            cy.get("input[placeholder='Password']").type(userdata.password)
+            cy.get("button[type='submit']").click()
+            cy.get(':nth-child(2) > .oxd-main-menu-item').contains(userdata.menu_expected)
+        
+        
 
     })
 
