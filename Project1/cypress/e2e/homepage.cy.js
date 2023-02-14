29edad8ac13f235b8f1fdf4de34388b5d7f0a908
officialpradip <study.pradipthapa@gmail.com>
2023-02-14 14:00:44 +0545

    beforeeach homepage to login

--- Project1/cypress/e2e/homepage.cy.js
+++ Project1/cypress/e2e/homepage.cy.js
@@ -1,8 +1,14 @@
 /// <reference types="Cypress"/>
 
 describe("CURA Health Care Services Testing Project",()=>{
-    it("HomePage",()=>{
+    beforeEach("Beginning",()=>{
         cy.visit("https://katalon-demo-cura.herokuapp.com/")
         cy.title().should('include','CURA Healthcare Service')
+        cy.get("#btn-make-appointment").click()
+        cy.get("div[class='col-sm-12 text-center'] h2").should('have.text','Login')
+    })
+    //login
+    it("Login",()=>{
+        
     })
 })
