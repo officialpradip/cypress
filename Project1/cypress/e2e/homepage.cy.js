12944bafefca85e69b76f75aaed281924171f4ba
officialpradip <study.pradipthapa@gmail.com>
2023-02-15 14:45:00 +0545

    button

--- Project1/cypress/e2e/homepage.cy.js
+++ Project1/cypress/e2e/homepage.cy.js
@@ -6,12 +6,16 @@ describe("CURA Health Care Services Testing Project",()=>{
         cy.title().should('include','CURA Healthcare Service')
         cy.get("#btn-make-appointment").click()
         cy.get("div[class='col-sm-12 text-center'] h2").should('have.text','Login')
-    })
-    //login
-    it("Login",()=>{
         cy.get('[name="username"]').type('John Doe')
         cy.get('[name="password"]').type('ThisIsNotAPassword')
         cy.get("#btn-login").click()
-        
+        cy.get("div[class='col-sm-12 text-center'] h2").should('have.text','Make Appointment')
+    })
+    
+    it("Test Facility",()=>{
+        cy.wait(3000)
+        cy.get('#combo_facility').select('Hongkong CURA Healthcare Center').should('have.value','Hongkong CURA Healthcare Center')
+        cy.get('#chk_hospotal_readmission').check() //check button
+        cy.get('[name="programs"]').check('Medicaid') //radio button
     })
 })
