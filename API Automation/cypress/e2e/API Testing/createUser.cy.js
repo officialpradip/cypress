5b9c9f2fb8498b1764c2f071f7da0df7208897f9
officialpradip <study.pradipthapa@gmail.com>
2023-02-27 13:57:08 +0545

    random email generation

--- API Automation/cypress/e2e/API Testing/createUser.cy.js
+++ API Automation/cypress/e2e/API Testing/createUser.cy.js
@@ -1,7 +1,15 @@
 ///<reference types="Cypress"/>
 describe("Create New User Testing",()=>{
    // let accessToken="904f350af861df3a884c4f80b4d96cdbc12e18b1df2a6b3f2461559148c65e66"
+   let randomEmail=""
+   let randomText=""
     it("Create User",()=>{
+        var emailpattern="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        for(var i=0;i<9;i++){
+            randomText+=emailpattern.charAt(Math.floor(Math.random()*emailpattern.length));
+            randomEmail=randomText + "@gmail.com";
+
+        }
         cy.request({
             method:'POST',
             url:'https://gorest.co.in/public/v2/users',
@@ -10,14 +18,15 @@ describe("Create New User Testing",()=>{
             },
             body:{
                 "name":'Pradip Thapa',
-                "email":'try123@gmail.com',
+                "email":randomEmail,
                 "gender":"Male",
                 "status":"active"
 
             }
         }).then((res)=>{
+            cy.log(JSON.stringify(res))
             expect(res.status).to.eq(201)
-            expect(res.body).has.property('email','try123@gmail.com')
+            expect(res.body).has.property('email',randomEmail)
         })
     })
 })
