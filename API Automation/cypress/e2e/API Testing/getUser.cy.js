294abeca38e60b67204531e2656d2c9f50540d09
officialpradip <study.pradipthapa@gmail.com>
2023-02-27 13:15:39 +0545

    get api

--- API Automation/cypress/e2e/API Testing/getUser.cy.js
+++ API Automation/cypress/e2e/API Testing/getUser.cy.js
@@ -11,4 +11,17 @@ describe("API Testing Practice",()=>{
             expect(req.status).to.eq(200)
         })
     })
+    it("Get User by id",()=>{
+        cy.request({
+            method:"GET",
+            url:"https://gorest.co.in/public/v2/users/662149",
+            headers:{
+                'authorization':"Bearer 904f350af861df3a884c4f80b4d96cdbc12e18b1df2a6b3f2461559148c65e66"
+            }
+        }).then((req)=>{
+            expect(req.status).to.eq(200)
+            
+        })
+    
+    })
 })
