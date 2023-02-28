7c1d3ac2a1285982d9b06e9e52fd38c41bf03894
officialpradip <study.pradipthapa@gmail.com>
2023-02-28 10:52:24 +0545

    create post setup

---
+++ API Automation/cypress/e2e/API Testing/createPost.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress"/>
+describe("Create Post Testing",()=>{
+    it("Create Post",()=>{
+        cy.request({
+            
+
+        }).then((req)=>{
+
+        })
+    })
+})
