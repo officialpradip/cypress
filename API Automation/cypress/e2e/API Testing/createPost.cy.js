019f1be0a295e1c75c5f8f7614d3a2b0da445f22
officialpradip <study.pradipthapa@gmail.com>
2023-02-28 10:58:11 +0545

    post failed

--- API Automation/cypress/e2e/API Testing/createPost.cy.js
+++ API Automation/cypress/e2e/API Testing/createPost.cy.js
@@ -1,10 +1,22 @@
 /// <reference types="Cypress"/>
 describe("Create Post Testing",()=>{
+    let accessToken="904f350af861df3a884c4f80b4d96cdbc12e18b1df2a6b3f2461559148c65e66";
     it("Create Post",()=>{
         cy.request({
-            
+            method:'POST',
+            url:'https://gorest.co.in/public/v2/posts',
+            headers:{
+                "Authorization":"Bearer" + accessToken
+            },
+            body:{
+                "user_id":687509,
+                "title":"Demo Test",
+                "body":"This is my first post"
 
+            }
         }).then((req)=>{
+            expect(req.status).to.be(401)
+            expect(req.body).has.property("title","Demo Test")
 
         })
     })
