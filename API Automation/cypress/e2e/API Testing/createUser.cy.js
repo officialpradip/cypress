dec2e16608cd02cd9c5ad2c5fb6b0d083aba4ae8
officialpradip <study.pradipthapa@gmail.com>
2023-02-28 11:23:01 +0545

    psot and get call

--- API Automation/cypress/e2e/API Testing/createUser.cy.js
+++ API Automation/cypress/e2e/API Testing/createUser.cy.js
@@ -36,6 +36,21 @@ describe("Create New User Testing",()=>{
             cy.log(JSON.stringify(res))
             expect(res.status).to.eq(201)
             expect(res.body).has.property('email',randomEmail)
+        }).then((res)=>{
+            //get call after user created
+            const userId=res.body.id;
+            cy.log("create user id is :::"+ userId)
+            cy.request({
+                method:'GET',
+                url:'https://gorest.co.in/public/v2/users/'+userId,
+            headers:{
+                'Authorization':"Bearer 904f350af861df3a884c4f80b4d96cdbc12e18b1df2a6b3f2461559148c65e66"
+            }
+            }).then((res)=>{
+                expect(res.status).to.eq(200)
+                expect(res.body).has.property('id',userId)
+                expect(res.body).has.property('email',randomEmail)
+            })
         })
     })
 })
