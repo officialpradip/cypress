988a17324951ed5d7df6ddc9efd4ad994a428d36
officialpradip <study.pradipthapa@gmail.com>
2023-05-01 13:48:03 +0545

    HTTP REQUEST

--- API Automation2/cypress/e2e/spec.cy.js
+++ API Automation2/cypress/e2e/spec.cy.js
@@ -14,4 +14,55 @@ describe("API TESTING",()=>{
             expect(res.status).to.eq(200)
         })
     })
+
+    it("POST CALL",()=>{
+        cy.request({
+            method:'POST',
+            url:'https://jsonplaceholder.typicode.com/posts',
+            body:{
+                
+                    userId: 100,
+                    title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                    body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+                  
+
+            }
+        }
+        ).its('status')
+        .should('equal',201)
+    })
+
+    it("PUT CALL",()=>{
+        cy.request({
+            method:'PUT',
+            url:'https://jsonplaceholder.typicode.com/posts/1',
+            body:{
+                
+                    userId: 100,
+                    title: "put call sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                    body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+                  
+
+            }
+        }
+        ).its('status')
+        .should('equal',200)
+    })
+
+    it("DELETE CALL",()=>{
+        cy.request({
+            method:'DELETE',
+            url:'https://jsonplaceholder.typicode.com/posts/1',
+            body:{
+                
+                    userId: 100,
+                    title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                    body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+                  
+
+            }
+        }
+        ).its('status')
+        .should('equal',200)
+    })
 })
