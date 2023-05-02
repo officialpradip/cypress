c798deb70435ca780e7c0f9a98cae34b833d0342
officialpradip <study.pradipthapa@gmail.com>
2023-05-02 18:02:45 +0545

    dynamic json value

--- API Automation2/cypress/e2e/posthardcode.cy.js
+++ API Automation2/cypress/e2e/posthardcode.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress"/>
 
 describe("API POST Testing",()=>{
-  it("hARD cODED",()=>{
+  it.skip("hARD cODED",()=>{
 
     const requestBody={
         tourist_name:"Pradip Thapa",
@@ -16,6 +16,23 @@ describe("API POST Testing",()=>{
         expect(req.status).to.eq(201)
         expect(req.body.tourist_name).to.eq("Pradip Thapa")
         expect(req.body).has.property("tourist_location","Kathmandu")
+    })
+  })
+  it("Dynamically Generated",()=>{
+
+    const requestBody={
+        tourist_name:Math.random().toString(5).substring(2),
+        tourist_email:Math.random().toString(5).substring(2)+"@gmail.com",
+        tourist_location:"Kathmandu"
+    }
+    cy.request({
+        method:'POST',
+        url:'http://restapi.adequateshop.com/api/Tourist',
+        body:requestBody
+    }).then((req)=>{
+        expect(req.status).to.eq(201)
+        expect(req.body.tourist_name).to.eq(requestBody.tourist_name)
+        expect(req.body).has.property("tourist_location",requestBody.tourist_location)
     })
   })
 })
