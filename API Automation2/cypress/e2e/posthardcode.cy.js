e4eb8e1633be352298988c51949c57e21da000a0
officialpradip <study.pradipthapa@gmail.com>
2023-05-02 18:14:54 +0545

    fixture response body

--- API Automation2/cypress/e2e/posthardcode.cy.js
+++ API Automation2/cypress/e2e/posthardcode.cy.js
@@ -3,6 +3,8 @@
 describe("API POST Testing",()=>{
   it.skip("hARD cODED",()=>{
 
+
+
     const requestBody={
         tourist_name:"Pradip Thapa",
         tourist_email:"pradip213@tourist.com",
@@ -18,7 +20,7 @@ describe("API POST Testing",()=>{
         expect(req.body).has.property("tourist_location","Kathmandu")
     })
   })
-  it("Dynamically Generated",()=>{
+  it.skip("Dynamically Generated",()=>{
 
     const requestBody={
         tourist_name:Math.random().toString(5).substring(2),
@@ -35,4 +37,20 @@ describe("API POST Testing",()=>{
         expect(req.body).has.property("tourist_location",requestBody.tourist_location)
     })
   })
+  it("Body response with Fixture",()=>{
+
+    // 
+    cy.fixture('demo').then((data)=>{
+        const requestBody=data;
+    cy.request({
+        method:'POST',
+        url:'http://restapi.adequateshop.com/api/Tourist',
+        body:requestBody
+    }).then((req)=>{
+        expect(req.status).to.eq(201)
+        expect(req.body.tourist_name).to.eq(requestBody.tourist_name)
+        expect(req.body).has.property("tourist_location",requestBody.tourist_location)
+    })
+  })
+})
 })
