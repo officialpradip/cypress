c8c6303ddc3b5c990f4418fb9ad239bb2c3be433
officialpradip <study.pradipthapa@gmail.com>
2023-05-02 17:49:32 +0545

    hard coded body response

--- API Automation2/cypress/e2e/posthardcode.cy.js
+++ API Automation2/cypress/e2e/posthardcode.cy.js
@@ -1,17 +1,21 @@
 /// <reference types="Cypress"/>
 
 describe("API POST Testing",()=>{
+  it("hARD cODED",()=>{
+
     const requestBody={
         tourist_name:"Pradip Thapa",
-        tourist_email:"pradip@tourist.com",
+        tourist_email:"pradip213@tourist.com",
         tourist_location:"Kathmandu"
     }
-
     cy.request({
         method:'POST',
         url:'http://restapi.adequateshop.com/api/Tourist',
         body:requestBody
     }).then((req)=>{
-        expect(req.status).to.eq(200)
+        expect(req.status).to.eq(201)
+        expect(req.body.tourist_name).to.eq("Pradip Thapa")
+        expect(req.body).has.property("tourist_location","Kathmandu")
     })
+  })
 })
